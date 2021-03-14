786416cebe406c74005aeac7fa787cf786d8f84d
RaimondasVee <44209928+RaimondasVee@users.noreply.github.com>
2021-03-14 16:16:03 +0000

    Edit comment

--- readCsv.php
+++ readCsv.php
@@ -15,13 +15,16 @@ if (($handle = fopen("Services.csv", "r")) !== FALSE) {
     // retrieves data from csv file and adds into a multidimensional array
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 
-        // Ignore first line of csv
-        if($flag) { $flag = false; continue; }  
+        // Ignore first line of csv acting as data headers
+        if($flag) { 
+            $flag = false; 
+            continue; 
+        }  
 
         // 3rd $data key of an array represents the 4th column of CSV file 
         // This can have a variable assigned to iterate different columns called by names.
         // Case insensitive argument accepted following this php call.
-        if(strtolower($data[3]) == strtolower($argv[1]))
+        if (strtolower($data[3]) == strtolower($argv[1]))
 
             //The data matching argument is pushed into a variable ready for iteration.
             array_push($result, $data);
